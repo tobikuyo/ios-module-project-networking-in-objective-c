b49001904923d803a2167ed4d11ad5fffedb0318
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-14 13:11:18 +0100

    Custom Initaliser for Weather Model in the Implementation File Setup

--- DailyWeather/Models/LSIWeather.m
+++ DailyWeather/Models/LSIWeather.m
@@ -10,4 +10,35 @@
 
 @implementation LSIWeather
 
+- (instancetype)initWithTime:(NSDate *)time
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+             precipIntensity:(double)precipIntensity
+           precipProbability:(double)precipProbability
+                 temperature:(double)temperature
+         apparentTemperature:(double)apparentTemperature
+                    humidity:(double)humidity
+                    pressure:(double)pressure
+                   windspeed:(double)windspeed
+                 windBearing:(NSNumber *)windBearing
+                     uvIndex:(NSNumber *)uvIndex {
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _precipIntesity = precipIntensity;
+        _precipProbability = precipProbability;
+        _temperature = temperature;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windspeed = windspeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+
+    return self;
+}
+
 @end
