0dda746ec91faa17fe79d6c4e17e8ab25c558425
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-15 14:20:00 +0100

    Weather Forecast Setup

--- DailyWeather/Models/LSIWeatherForecast.h
+++ DailyWeather/Models/LSIWeatherForecast.h
@@ -7,10 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSICurrentWeather.h"
+#import "LSIDailyForecast.h"
+#import "LSIHourlyForecast.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIWeatherForecast : NSObject
+
+@property LSICurrentWeather *currentWeather;
+@property NSArray<LSIDailyForecast *> *dailyForecasts;
+@property NSArray<LSIHourlyForecast *> *hourlyForecasts;
+
+-(instancetype)initWithCurrentWeather:(LSICurrentWeather *)currentWeather
+                       dailyForecasts:(NSArray *)dailyForecasts
+                      hourlyForecasts:(NSArray *)hourlyForecasts;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
 
--- DailyWeather/Models/LSIWeatherForecast.m
+++ DailyWeather/Models/LSIWeatherForecast.m
@@ -10,4 +10,33 @@
 
 @implementation LSIWeatherForecast
 
+-(instancetype)initWithCurrentWeather:(LSICurrentWeather *)currentWeather
+                       dailyForecasts:(NSArray *)dailyForecasts
+                      hourlyForecasts:(NSArray *)hourlyForecasts {
+    self = [super init];
+    if (self) {
+        _currentWeather = currentWeather;
+        _dailyForecasts = dailyForecasts;
+        _hourlyForecasts = hourlyForecasts;
+    }
+
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    LSICurrentWeather *currentWeather = dictionary[@"currently"];
+
+    NSDictionary *daily = dictionary[@"daily"];
+    NSArray *dailyForecasts = daily[@"data"];
+
+    NSDictionary *hourly = dictionary[@"hourly"];
+    NSArray *hourlyForecasts = hourly[@"data"];
+
+    self = [self initWithCurrentWeather:currentWeather
+                         dailyForecasts:dailyForecasts
+                        hourlyForecasts:hourlyForecasts];
+
+    return self;
+}
+
 @end
