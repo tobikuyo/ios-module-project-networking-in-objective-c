ef441cb8e88b2f5335a5ce1e8d96fc8da2e016e9
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-15 14:00:59 +0100

    Hourly Forecast

--- DailyWeather/Models/LSIHourlyForecast.m
+++ DailyWeather/Models/LSIHourlyForecast.m
@@ -10,4 +10,130 @@
 
 @implementation LSIHourlyForecast
 
+- (instancetype)initWithTime:(NSDate *)time
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+             precipIntensity:(double)precipIntensity
+           precipProbability:(double)precipProbability
+                  precipType:(double)precipType
+              temperatureLow:(double)temperatureLow
+             temperatureHigh:(double)temperatureHigh
+         apparentTemperature:(double)apparentTemperature
+                    humidity:(double)humidity
+                    pressure:(double)pressure
+                   windspeed:(double)windspeed
+                 windBearing:(NSNumber *)windBearing
+                     uvIndex:(NSNumber *)uvIndex {
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _precipIntesity = precipIntensity;
+        _precipProbability = precipProbability;
+        _precipType = precipType;
+        _temperatureLow = temperatureLow;
+        _temperatureHigh = temperatureHigh;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windspeed = windspeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    NSNumber *timeNumber = dictionary[@"time"];
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:timeNumber.longValue];
+    NSString *summary = dictionary[@"summary"];
+    NSString *icon = dictionary[@"icon"];
+    NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+    NSNumber *precipProbability = dictionary[@"precipProbability"];
+    NSNumber *precipType = dictionary[@"precipType"];
+    NSNumber *temperatureLow = dictionary[@"temperatureLow"];
+    NSNumber *temperatureHigh = dictionary[@"temperatureHigh"];
+    NSNumber *apparentTemperature = dictionary[@"apparentTemperature"];
+    NSNumber *humidity = dictionary[@"humidity"];
+    NSNumber *pressure = dictionary[@"pressure"];
+    NSNumber *windSpeed = dictionary[@"windSpeed"];
+    NSNumber *windBearing = dictionary[@"windBearing"];
+    NSNumber *uvIndex = dictionary[@"uvIndex"];
+
+    if (!timeNumber) {
+        return nil;
+    }
+
+    if ([summary isKindOfClass:[NSNull class]]) {
+        summary = nil;
+    }
+
+    if ([icon isKindOfClass:[NSNull class]]) {
+        icon = nil;
+    }
+
+    if ([precipProbability isKindOfClass:[NSNull class]]) {
+        precipProbability = nil;
+    }
+
+    if ([precipIntensity isKindOfClass:[NSNull class]]) {
+        precipIntensity = nil;
+    }
+
+    if ([precipType isKindOfClass:[NSNull class]]) {
+        precipType = nil;
+    }
+
+    if ([temperatureLow isKindOfClass:[NSNull class]]) {
+        temperatureLow = nil;
+    }
+
+    if ([temperatureHigh isKindOfClass:[NSNull class]]) {
+        temperatureHigh = nil;
+    }
+
+    if ([apparentTemperature isKindOfClass:[NSNull class]]) {
+        apparentTemperature = nil;
+    }
+
+    if ([humidity isKindOfClass:[NSNull class]]) {
+        humidity = nil;
+    }
+
+    if ([pressure isKindOfClass:[NSNull class]]) {
+        pressure = nil;
+    }
+
+    if ([windSpeed isKindOfClass:[NSNull class]]) {
+        windSpeed = nil;
+    }
+
+    if ([windBearing isKindOfClass:[NSNull class]]) {
+        windBearing = nil;
+    }
+
+    if ([uvIndex isKindOfClass:[NSNull class]]) {
+        uvIndex = nil;
+    }
+
+    self = [self initWithTime:time
+                      summary:summary
+                         icon:icon
+              precipIntensity:precipIntensity.doubleValue
+            precipProbability:precipProbability.doubleValue
+                   precipType:precipType.doubleValue
+               temperatureLow:temperatureLow.doubleValue
+              temperatureHigh:temperatureHigh.doubleValue
+          apparentTemperature:apparentTemperature.doubleValue
+                     humidity:humidity.doubleValue
+                     pressure:pressure.doubleValue
+                    windspeed:windSpeed.doubleValue
+                  windBearing:windBearing
+                      uvIndex:uvIndex];
+
+    return self;
+}
+
 @end
